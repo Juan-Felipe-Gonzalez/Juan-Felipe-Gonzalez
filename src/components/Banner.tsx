282b69b61b41dcbf28b17e912736a45e27f26fba
Dan802 <jfelipeg05@gmail.com>
2026-02-22 19:26:25 -0500

    github added to home

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -2,6 +2,7 @@ import { useApp } from "../context/AppContext";
 import { translations } from "../data/translations";
 import fondo3Img from "../assets/img/Fondo3.webp";
 import linkedinSmallImg from "../assets/img/Logos/linkedinSmall.webp";
+import gitHub_Lockup from "../assets/img/Logos/gitHub_Lockup.webp";
 
 export default function Banner() {
   const { language } = useApp();
@@ -20,51 +21,31 @@ export default function Banner() {
           {translation.developer.title}
         </h2>
 
-        <div className="w-[60%] md:flex justify-start gap-12">
+        <div className="md:flex justify-start gap-12">
           <a
-            className="flex items-center justify-center p-8 bg-[#061221] rounded-lg shadow-[0_5px_9px_hsla(0,0%,100%,0.35)]"
+            className="flex items-center justify-center p-8 border-[#20202e] bg-[#111118] rounded-4xl gap-4 shadow-[0_5px_9px_hsla(0,0%,100%,0.35)]"
             href="https://www.linkedin.com/in/juan-felipe-gonzalez-fullstack/"
             target="_blank"
-            rel="noopener noreferrer"
           >
             <img
               src={linkedinSmallImg}
               alt="LinkedIn Logo"
-              height="29"
-              width="102"
+              className="h-auto w-60"
             />
-            &nbsp;&nbsp;
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              height="1em"
-              viewBox="0 0 512 512"
-              className="fill-white"
-            >
-              <path d="M320 0c-17.7 0-32 14.3-32 32s14.3 32 32 32h82.7L201.4 265.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L448 109.3V192c0 17.7 14.3 32 32 32s32-14.3 32-32V32c0-17.7-14.3-32-32-32H320zM80 32C35.8 32 0 67.8 0 112V432c0 44.2 35.8 80 80 80H400c44.2 0 80-35.8 80-80V320c0-17.7-14.3-32-32-32s-32 14.3-32 32V432c0 8.8-7.2 16-16 16H80c-8.8 0-16-7.2-16-16V112c0-8.8 7.2-16 16-16H192c17.7 0 32-14.3 32-32s-14.3-32-32-32H80z" />
-            </svg>
+            <i className="fa-solid fa-arrow-up-right-from-square text-white text-3xl"></i>
           </a>
           
           <a
-            className="flex items-center justify-center p-8 bg-[#061221] rounded-lg shadow-[0_5px_9px_hsla(0,0%,100%,0.35)]"
-            href="https://www.linkedin.com/in/juan-felipe-gonzalez-fullstack/"
+            className="flex items-center justify-center p-8 border-[#20202e] bg-[#111118] rounded-4xl gap-4 shadow-[0_5px_9px_hsla(0,0%,100%,0.35)]"
+            href="https://github.com/Juan-Felipe-Gonzalez"
             target="_blank"
-            rel="noopener noreferrer"
           >
             <img
-              src={linkedinSmallImg}
-              alt="LinkedIn Logo"
-              height="29"
-              width="102"
+              src={gitHub_Lockup}
+              alt="github Logo"
+              className="h-auto w-60"
             />
-            &nbsp;&nbsp;
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              height="1em"
-              viewBox="0 0 512 512"
-              className="fill-white"
-            >
-              <path d="M320 0c-17.7 0-32 14.3-32 32s14.3 32 32 32h82.7L201.4 265.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L448 109.3V192c0 17.7 14.3 32 32 32s32-14.3 32-32V32c0-17.7-14.3-32-32-32H320zM80 32C35.8 32 0 67.8 0 112V432c0 44.2 35.8 80 80 80H400c44.2 0 80-35.8 80-80V320c0-17.7-14.3-32-32-32s-32 14.3-32 32V432c0 8.8-7.2 16-16 16H80c-8.8 0-16-7.2-16-16V112c0-8.8 7.2-16 16-16H192c17.7 0 32-14.3 32-32s-14.3-32-32-32H80z" />
-            </svg>
+            <i className="fa-solid fa-arrow-up-right-from-square text-white text-3xl"></i>
           </a>
         </div>
       </div>
--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -12,35 +12,35 @@ export default function Skills() {
     <main className="w-[95%] max-w-[1200px] mx-auto lg:w-full" id='stack'>
       <div className='flex flex-wrap m-auto justify-center'>
           <button 
-            className={`inline-block border cursor-pointer shadow-lg shadow-[#151530]/50 p-6 m-1 rounded-xl text-white ${skillSelected == translation.titlesSkills.frontend ? 'border-[#21215b] bg-[#151530]' : 'border-[#20202e] bg-[#111118]'}`}
+            className={`border cursor-pointer shadow-lg shadow-[#151530]/50 p-6 m-1 rounded-xl text-white  ${skillSelected == translation.titlesSkills.frontend ? 'border-[#21215b] bg-[#151530] flex-1' : 'border-[#20202e] bg-[#111118] flex'}`}
             onClick={() => setskillSelected(translation.titlesSkills.frontend)}
             >
               <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="inline mr-1 icon icon-tabler icons-tabler-outline icon-tabler-user-screen"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M19.03 17.818a3 3 0 0 0 1.97 -2.818v-8a3 3 0 0 0 -3 -3h-12a3 3 0 0 0 -3 3v8c0 1.317 .85 2.436 2.03 2.84" /><path d="M10 14a2 2 0 1 0 4 0a2 2 0 0 0 -4 0" /><path d="M8 21a2 2 0 0 1 2 -2h4a2 2 0 0 1 2 2" /></svg>
               {translation.titlesSkills.frontend}
           </button>
           <button 
-            className={`inline-block border cursor-pointer shadow-lg shadow-[#151530]/50 p-6 m-1 rounded-xl text-white ${skillSelected == translation.titlesSkills.backend ? 'border-[#21215b] bg-[#151530]' : 'border-[#20202e] bg-[#111118]'}`}
+            className={`border cursor-pointer shadow-lg shadow-[#151530]/50 p-6 m-1 rounded-xl text-white ${skillSelected == translation.titlesSkills.backend ? 'border-[#21215b] bg-[#151530] flex-1' : 'border-[#20202e] bg-[#111118] flex'}`}
             onClick={() => setskillSelected(translation.titlesSkills.backend)}
             >
               <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="inline mr-1 icon icon-tabler icons-tabler-outline icon-tabler-database"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M4 6a8 3 0 1 0 16 0a8 3 0 1 0 -16 0" /><path d="M4 6v6a8 3 0 0 0 16 0v-6" /><path d="M4 12v6a8 3 0 0 0 16 0v-6" /></svg>
               {translation.titlesSkills.backend}
           </button>
           <button 
-            className={`inline-block border cursor-pointer shadow-lg shadow-[#151530]/50 p-6 m-1 rounded-xl text-white ${skillSelected == translation.titlesSkills.cloud ? 'border-[#21215b] bg-[#151530]' : 'border-[#20202e] bg-[#111118]'}`}
+            className={`border cursor-pointer shadow-lg shadow-[#151530]/50 p-6 m-1 rounded-xl text-white ${skillSelected == translation.titlesSkills.cloud ? 'border-[#21215b] bg-[#151530] flex-1' : 'border-[#20202e] bg-[#111118] flex'}`}
             onClick={() => setskillSelected(translation.titlesSkills.cloud)}
             >
               <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="inline mr-1 icon icon-tabler icons-tabler-outline icon-tabler-cloud"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M6.657 18c-2.572 0 -4.657 -2.007 -4.657 -4.483c0 -2.475 2.085 -4.482 4.657 -4.482c.393 -1.762 1.794 -3.2 3.675 -3.773c1.88 -.572 3.956 -.193 5.444 1c1.488 1.19 2.162 3.007 1.77 4.769h.99c1.913 0 3.464 1.56 3.464 3.486c0 1.927 -1.551 3.487 -3.465 3.487h-11.878" /></svg> 
               {translation.titlesSkills.cloud}
           </button>
           <button 
-            className={`inline-block border cursor-pointer shadow-lg shadow-[#151530]/50 p-6 m-1 rounded-xl text-white ${skillSelected == translation.titlesSkills.design ? 'border-[#21215b] bg-[#151530]' : 'border-[#20202e] bg-[#111118]'}`}
+            className={`border cursor-pointer shadow-lg shadow-[#151530]/50 p-6 m-1 rounded-xl text-white ${skillSelected == translation.titlesSkills.design ? 'border-[#21215b] bg-[#151530] flex-1' : 'border-[#20202e] bg-[#111118] flex'}`}
             onClick={() => setskillSelected(translation.titlesSkills.design)}
             >
               <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="inline mr-1 icon icon-tabler icons-tabler-outline icon-tabler-pencil"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M4 20h4l10.5 -10.5a2.828 2.828 0 1 0 -4 -4l-10.5 10.5v4" /><path d="M13.5 6.5l4 4" /></svg> 
               {translation.titlesSkills.design}
           </button>
           <button 
-            className={`inline-block border cursor-pointer shadow-lg shadow-[#151530]/50 p-6 m-1 rounded-xl text-white ${skillSelected == translation.titlesSkills.languages ? 'border-[#21215b] bg-[#151530]' : 'border-[#20202e] bg-[#111118]'}`}
+            className={`border cursor-pointer shadow-lg shadow-[#151530]/50 p-6 m-1 rounded-xl text-white ${skillSelected == translation.titlesSkills.languages ? 'border-[#21215b] bg-[#151530] flex-1' : 'border-[#20202e] bg-[#111118] flex'}`}
             onClick={() => setskillSelected(translation.titlesSkills.languages)}
             
             >
